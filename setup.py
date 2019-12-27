cb54ccb043b09d40884e1d912bbfb9ac4fc98e76
Bendik Samseth <b.samseth@gmail.com>
2019-12-27 10:46:54 +0100

    Update setup.py so this fork can be on PyPI

--- setup.py
+++ setup.py
@@ -1,24 +1,30 @@
 """Install jsonschema-typed."""
 
-from setuptools import setup, find_packages
+from setuptools import setup
 import os
 
-dir = os.path.abspath(os.path.dirname(__file__))
+repo_base_dir = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(dir, "README.md")) as f:
+with open(os.path.join(repo_base_dir, "README.md")) as f:
     description = f.read()
 
 setup(
-    name="jsonschema-typed",
-    author="Erick Peirson",
-    author_email="erick.peirson@gmail.com",
-    url="https://github.com/erickpeirson/jsonschema-typed",
-    python_requires="~=3.6",  # I think?
+    name="jsonschema-typed-v2",
+    author="Bendik Samseth",
+    author_email="b.samseth@gmail.com",
+    url="https://github.com/bsamseth/jsonschema-typed",
+    python_requires=">=3.8",  # At least to develop, possibly importable in lower versions?
     license="MIT",
-    version="0.1.0",
+    version="0.2.0",
     packages=["jsonschema_typed"],
     zip_safe=False,
-    install_requires=["jsonschema>=3.0.1", "mypy>=0.701"],
-    long_description=description,
+    install_requires=[
+        "jsonschema>=3.2.0",
+        "mypy>=0.761",
+    ],  # Possibly not so strict, but don't want to check.
+    long_description=(
+        """This is a fork of Erick Peirson's original `jsonschema_typed`.\n\n"""
+        + description
+    ),
     long_description_content_type="text/markdown",
 )
